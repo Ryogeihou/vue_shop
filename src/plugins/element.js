561ba2a7ee53fca2efad3666334a3fc2e947c210
ryogeihou <ryogeihou@gmail.com>
2021-08-12 03:10:22 +0900

    rights

--- src/plugins/element.js
+++ src/plugins/element.js
@@ -16,7 +16,9 @@ import {
   Tooltip,
   Pagination,
   Dialog,
-  MessageBox
+  MessageBox,
+  Tag,
+  Tree
 } from 'element-ui'
 
 Vue.use(Button)
@@ -42,5 +44,7 @@ Vue.use(Switch)
 Vue.use(Tooltip)
 Vue.use(Pagination)
 Vue.use(Dialog)
+Vue.use(Tag)
+Vue.use(Tree)
 Vue.prototype.$message = Message
 Vue.prototype.$confirm = MessageBox.confirm
